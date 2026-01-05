708e07c79e55a4dff822965faa8ad5c9da0932e2
Baykov Ivan <45362427+Efcolipt@users.noreply.github.com>
2026-01-05 15:53:57 +0300

    chore: hash set

--- src/bin/hash_set.rs
+++ src/bin/hash_set.rs
@@ -1,1 +1,16 @@
-fn 
+#![allow(unused)]
+
+use std::collections::HashSet;
+
+fn main() {
+    let mut set: HashSet<u32> = HashSet::new();
+
+    let inserted = set.insert(1);
+    println!("{inserted}");
+
+    let contains = set.contains(&1);
+    println!("contains1 {contains}");
+
+    let contains = set.contains(&2);
+    println!("contains2 {contains}");
+}
