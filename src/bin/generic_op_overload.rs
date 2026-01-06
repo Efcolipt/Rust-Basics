52f91884ecf9c6cd4100ce002666774a00f4abc3
Baykov Ivan <45362427+Efcolipt@users.noreply.github.com>
2026-01-06 10:35:49 +0300

    chore: add generic operators overload

---
+++ src/bin/generic_op_overload.rs
@@ -0,0 +1,32 @@
+#![allow(unused)]
+use std::ops::Add;
+
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Add for Point<T>
+where
+    T: Add<Output = T>,
+{
+    type Output = Point<T>;
+
+    fn add(self, rhs: Point<T>) -> Self::Output {
+        Point {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+fn main() {
+    let p0 = Point { x: 1, y: 1 };
+
+    let p1 = Point { x: 1, y: 1 };
+
+    let p2 = p0 + p1;
+
+    println!("{:?}", p2)
+}
